85e515babbd6cb4dea19d636c17f510419bf99f8
Andrew Antonov <antonov.andrew.jr@gmail.com>
2022-08-19 17:31:21 +0500

    mobile menu animation

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames'
 
 import React, { useState } from 'react'
 import { useRouter } from 'next/router'
+import { useSpring, animated } from 'react-spring'
 import type { FunctionComponent } from 'react'
 
 import Link from 'next/link'
@@ -15,12 +16,19 @@ import navItems from '../constants/navItems'
 const MobileMenu: FunctionComponent<DefaultProp>  = ({ className } : DefaultProp) => {
     const router = useRouter()
     const [isOpen, setIsOpen] = useState(false)
+    const styles = useSpring({ 
+        height: isOpen ? '100vh' : '0vh',
+        config: {
+            mass: 5,
+            duration: 350
+        }
+    })
 
     return (
         <div className={classNames(className, '')}>
             { 
                 isOpen ? 
-                <div className={classNames(className, 'w-full h-screen flex flex-col items-center bg-black py-20 justify-between')}>  
+                <animated.div style={styles} className={classNames(className, 'w-full h-screen flex flex-col items-center bg-black py-20 justify-between')}>
                     <AncubedLogo />
                     <nav className='flex flex-col items-center font-light text-3xl -my-5'>
                         {navItems.map(item => 
@@ -35,7 +43,7 @@ const MobileMenu: FunctionComponent<DefaultProp>  = ({ className } : DefaultProp
                     <button className='font-light text-3xl' onClick={() => setIsOpen(false)}>
                         Close
                     </button>
-                </div>
+                </animated.div>
                 : 
                 <button className='font-light text-3xl w-full justify-center mt-10' onClick={() => setIsOpen(true)}>
                     Menu
