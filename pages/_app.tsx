8130db33c3f4b681ced8639d96b8dc2c63bf3b53
Andrew Antonov <antonov.andrew.jr@gmail.com>
2022-08-22 16:46:31 +0500

    удален footer и отформатирован myapp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,10 @@ import '../styles/globals.css'
 import React from 'react'
 
 import Head from '../components/Head'
-import Footer from '../components/Footer'
 import Menu from '../components/Menu'
 import MobileMenu from '../components/MobileMenu'
+import AncubedLogo from '../components/AncubedLogo'
+
 import Background from '../blackhole/BlackHoleApp'
 
 import type { AppProps } from 'next/app'
@@ -15,13 +16,16 @@ function MyApp({ Component, pageProps }: AppProps) {
   return <>
       <Head />
 
-      <main className='z-10 absolute text-white font-sans w-full' >
-        <Menu className='hidden sm:flex sm:relative sm:top-20 sm:justify-center sm:w-full lg:w-min lg:inset-x-36'/>
-        <MobileMenu className='sm:hidden'/>
-        <Component {...pageProps} />
-      </main>
+      <header className='z-50 absolute w-full select-none text-white font-sans lg:w-fit lg:-mx-6'>
+          <Menu className='hidden sm:relative sm:top-20 sm:flex sm:justify-center sm:w-full lg:w-min lg:inset-x-36'/>
+          <MobileMenu className='sm:hidden'/>
+      </header>
 
-      <Footer className='z-10 hidden bottom-20 absolute text-white font-sans sm:flex sm:inset-x-1/2 sm:-translate-x-[50px] lg:transform-none lg:inset-x-auto lg:right-36'/>
+      <Component {...pageProps} className='z-40 absolute left-auto w-full select-none text-white font-sans lg:w-fit lg:left-36 lg:block'/>
+
+      <footer className={'z-40 absolute top-24 inset-x-1/2 translate-x-[3px] select-none text-white font-sans sm:bottom-20 sm:top-auto sm:flex sm:-translate-x-[77px] lg:right-36 lg:transform-none lg:inset-x-auto'}>
+          <AncubedLogo />
+      </footer>
 
       <Background />
   </>
