be9501b07a0850460589a75da87d39d002987fff
Andrew Antonov <antonov.andrew.jr@gmail.com>
2022-08-24 11:39:12 +0500

    zustand убраны девутлзы и исправлена ts ошибка

--- hooks/useLangStore.ts
+++ hooks/useLangStore.ts
@@ -1,23 +1,13 @@
 import create from 'zustand'
-import { devtools, persist } from 'zustand/middleware'
 
 interface LanguageState {
   enLang: boolean
   changeLang: () => void
 }
 
-const useLangStore = create<LanguageState>()(
-  devtools(
-    persist(
-      (set) => ({
-        enLang: true,
-        changeLang: () => set((state) => ({ enLang: !state.enLang })),
-      }),
-      {
-        name: 'language-storage',
-      }
-    )
-  )
-)
+const useLangStore = create<LanguageState>()((set) => ({
+  enLang: true,
+  changeLang: () => set((state) => ({ enLang: !state.enLang })),
+}))
 
 export default useLangStore
