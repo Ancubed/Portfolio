d8385a8659850d02838befe75e87ca9d21bf6d42
Andrew Antonov <antonov.andrew.jt@gmail.com>
2022-09-29 10:18:56 +0500

    prettier

--- components/Head.tsx
+++ components/Head.tsx
@@ -7,7 +7,10 @@ const HeadWrapper: FunctionComponent = () => {
   return (
     <Head>
       <title>Ancubed</title>
-      <meta name="description" content="Андрей Антонов. Fullstack-разработчик. Портфолио." />
+      <meta
+        name="description"
+        content="Андрей Антонов. Fullstack-разработчик. Портфолио."
+      />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
