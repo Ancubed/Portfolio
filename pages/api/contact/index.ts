ec78e2d37ada69ad63a3d57f14b0d9f47a1b5b82
Andrew Antonov <antonov.andrew.jt@gmail.com>
2022-09-29 09:33:42 +0500

    Добавлена поддержка бота

--- pages/api/contact/index.ts
+++ pages/api/contact/index.ts
@@ -1,12 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { DefaultResponce } from "../../../types/types";
 
+import { distributeMessage } from '../../../lib/bot'
+
 async function postContactHandler(
   req: NextApiRequest,
   res: NextApiResponse<DefaultResponce>
 ) {
-  console.log(req.body);
-  // Оправить на бота инфу
+  let { name, email, question } = req.body;
+  if (process.env.SIMPLE_ID && name && email && question) {
+    let mesResult = await distributeMessage(process.env.SIMPLE_ID, `${name} - Email: ${email} задал вопрос:\n${question}`);
+  }
   return res.status(200).json({ success: true, data: null });
 }
 
