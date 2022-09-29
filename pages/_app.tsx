f9cb8083c67c8853f74bff076b93b1652f690a9a
Andrew Antonov <antonov.andrew.jt@gmail.com>
2022-09-29 17:01:52 +0500

    yandex metrika добавлена поддержка роутинга

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import "../styles/globals.css";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
-import Script from "next/script";
+import ym, { YMInitializer }  from 'react-yandex-metrika';
 
 import Head from "../components/Head";
 import Menu from "../components/Menu";
@@ -20,6 +20,16 @@ import type { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const { width, height } = useScreenSize();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      ym('hit', url)
+    }
+    router.events.on('routeChangeStart', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange)
+    }
+  }, [])
 
   return (
     <>
@@ -52,22 +62,8 @@ function MyApp({ Component, pageProps }: AppProps) {
       )}
 
       <Background pathname={router.pathname} screenSizeWidth={width} />
-
-      <Script
-        dangerouslySetInnerHTML={{
-          __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();
-                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-                ym(90594452, "init", {
-                      clickmap:true,
-                      trackLinks:true,
-                      accurateTrackBounce:true
-                });`,
-        }}
-      />
+      
+      <YMInitializer accounts={[90594452]} />
     </>
   );
 }
