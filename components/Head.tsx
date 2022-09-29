2c2743f04067e135c48789e15aeb94059fae1590
Andrew Antonov <antonov.andrew.jt@gmail.com>
2022-09-29 10:08:42 +0500

    подправлен description и добавлен robots.txt

--- components/Head.tsx
+++ components/Head.tsx
@@ -7,7 +7,7 @@ const HeadWrapper: FunctionComponent = () => {
   return (
     <Head>
       <title>Ancubed</title>
-      <meta name="description" content="Антонов Андрей" />
+      <meta name="description" content="Андрей Антонов. Fullstack-разработчик. Портфолио." />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
